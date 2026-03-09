caf9eb915c62a79b4c8ebeede7559a08ee75d830
Kevin Langga <kikoirfan45@gmail.com>
2026-03-09 09:05:21 +0700

    Tugas Hampir Selesai

--- script.js
+++ script.js
@@ -19,6 +19,24 @@ const products = [
     price: "Rp 850.000",
     image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=400",
   },
+
+  {
+    name: "Charger Laptop",
+    price: "Rp 700.000",
+    image: "https://images.unsplash.com/photo-1756043827116-5764e6d23d85?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNoYXJnZXIlMjBsYXB0b3B8ZW58MHx8MHx8fDA%3D",
+  },
+
+  {
+    name: "Gaming Chair",
+    price: "Rp. 1.420.000",
+    image: "https://images.unsplash.com/photo-1770194993269-2521ad916c23?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fGdhbWluZyUyMGNoYWlyfGVufDB8fDB8fHww",
+  },
+
+  {
+    name: "CPU GAMING",
+    price: "Rp. 450.000",
+    image: "https://images.unsplash.com/photo-1658673609646-9c7ba9514b89?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FtaW5nJTIwY3B1fGVufDB8fDB8fHww",
+  }
 ];
 
 const container = document.getElementById("product-container");
@@ -41,12 +59,3 @@ products.forEach((item) => {
   container.appendChild(card);
 });
 
-for (let i = 0; i < products.length; i++) {
-    let stok = 0;
-
-    if (stok.item === 0) {
-
-    } else {
-        
-    }
-}
